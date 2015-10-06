aae92acc21f190eb6e94b5457abf0839d1ac52dd
Khaliq Gant <khaliqgant@gmail.com>
2015-10-06 16:55:40 -0400

    add in nvm and also WIP update_nvm function

--- zshes/path.zsh
+++ zshes/path.zsh
@@ -17,6 +17,7 @@ export DOCKER_HOST=tcp://192.168.59.103:2376
 export DOCKER_CERT_PATH=/Users/khaliq/.boot2docker/certs/boot2docker-vm
 export DOCKER_TLS_VERIFY=1
 
-# use nvm to manage node versions
-source /usr/local/opt/nvm/nvm.sh
+# nvm
+export NVM_DIR="/Users/khaliq/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
