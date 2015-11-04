8c61f9852dcfcc4d43dc14241ee634b590edc2bf
Khaliq Gant <khaliqgant@gmail.com>
2015-11-04 13:34:07 -0500

    move to slack for dev chat

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 Applications=/Applications/
 
 open $Applications/Google\ Chrome.app
-open $Applications/Hipchat.app
+open $Applications/Slack.app
 open $Applications/Spotify.app
 open $Applications/Eon.app
 open $Applications/iTerm.app
