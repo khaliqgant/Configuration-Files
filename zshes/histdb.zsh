467e0640f9a25e0b6a91f40b8f32d4e01568a3ae
Khaliq Gant <khaliqgant@gmail.com>
2017-08-23 13:51:06 +0200

    update histdb query to prioritize the pwd directory but also look at commands in other directories

--- zshes/histdb.zsh
+++ zshes/histdb.zsh
@@ -11,9 +11,12 @@ _zsh_autosuggest_strategy_histdb_top_here() {
     local query="select commands.argv from
 history left join commands on history.command_id = commands.rowid
 left join places on history.place_id = places.rowid
-where places.dir LIKE '$(sql_escape $PWD)%'
+where (places.dir LIKE '$(sql_escape $PWD)%' OR 1=1)
 and commands.argv LIKE '$(sql_escape $1)%'
-group by commands.argv order by count(*) desc limit 1"
+group by commands.argv 
+order by count(*) desc, 
+places.dir LIKE '$(sql_escape $PWD)%' desc 
+limit 1"
     _histdb_query "$query"
 }
 
