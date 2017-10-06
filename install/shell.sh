e43557e4d6b7ff31cf2ddb8b9a903831ddb8d1d4
Khaliq Gant <khaliqgant@gmail.com>
2017-10-06 13:28:24 +0200

    add zsh-nvm and set default node version in home

--- install/shell.sh
+++ install/shell.sh
@@ -4,5 +4,7 @@
 $dry curl http://j.mp/spf13-vim3 -L -o - | $dry sh
 echo "setting symlinks"
 sh symlinks.sh
-echo "adding zsh syntax highlighting"
-($dry cd ~/.oh-my-zsh/custom/plugins && $dry git clone git://github.com/zsh-users/zsh-syntax-highlighting.git)
+echo "adding zsh syntax highlighting and zsh-nvm"
+($dry cd ~/.oh-my-zsh/custom/plugins && 
+$dry git clone git://github.com/zsh-users/zsh-syntax-highlighting.git && 
+$dry git clone git@github.com:lukechilds/zsh-nvm.git)
