ec171e680bc6b0ddaa1183adb0532dace20253f8
Khaliq Gant <khaliqgant@gmail.com>
2018-01-06 17:56:46 +0100

    add in zsh plugin logic

--- install/shell.sh
+++ install/shell.sh
@@ -1,8 +1,14 @@
- echo "installing spf13"
+echo "installing spf13"
 $dry curl http://j.mp/spf13-vim3 -L -o - | $dry sh
 echo "setting symlinks"
 sh symlinks.sh
-echo "adding zsh syntax highlighting and zsh-nvm"
-($dry cd ~/.oh-my-zsh/custom/plugins && 
-$dry git clone git://github.com/zsh-users/zsh-syntax-highlighting.git && 
-$dry git clone git@github.com:lukechilds/zsh-nvm.git)
+echo "adding zsh plugins"
+zshes=$(<data/zshPlugins.txt)
+
+for zsh in $zshes
+do
+    ($dry cd ~/.oh-my-zsh/custom/plugins && $dry git clone $zsh)
+done
+
+echo "copying zsh config to plugins"
+$dry cp ~/Configuration-Files/install/config/zsh/* ~/.oh-my-zsh/custom/
