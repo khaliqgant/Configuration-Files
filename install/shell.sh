4fed8e7a42403fce332052cd4f7593e11177164b
Khaliq Gant <khaliqgant@gmail.com>
2018-01-06 18:23:30 +0100

    add command to make vimproc after sps is installed

--- install/shell.sh
+++ install/shell.sh
@@ -1,5 +1,6 @@
 echo "installing spf13"
 $dry curl http://j.mp/spf13-vim3 -L -o - | $dry sh
+($dry cd ~/.vim/bundle/vimproc.vim && make)
 echo "setting symlinks"
 sh symlinks.sh
 echo "adding zsh plugins"
