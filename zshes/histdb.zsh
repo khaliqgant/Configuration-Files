1e3896896f7b2301acaada9b904eb87342a39ffe
Khaliq Gant <khaliqgant@gmail.com>
2018-01-06 18:14:01 +0100

    fix up zsh suggestions

--- zshes/histdb.zsh
+++ zshes/histdb.zsh
@@ -7,6 +7,18 @@ autoload -Uz add-zsh-hook
 add-zsh-hook preexec _start_timer
 add-zsh-hook precmd  _stop_timer
 
+# Query suggested by the zsh-histdb repo
+_zsh_autosuggest_strategy_histdb_top() {
+    local query="select commands.argv from
+history left join commands on history.command_id = commands.rowid
+left join places on history.place_id = places.rowid
+where commands.argv LIKE '$(sql_escape $1)%'
+group by commands.argv
+order by places.dir != '$(sql_escape $PWD)', count(*) desc limit 1"
+    suggestion=$(_histdb_query "$query")
+}
+
+# Custom query to pull in secondary commands if none found given the pwd
 _zsh_autosuggest_strategy_histdb_top_here() {
     local query="select commands.argv from
 history left join commands on history.command_id = commands.rowid
@@ -17,7 +29,7 @@ group by commands.argv
 order by count(*) desc, 
 places.dir LIKE '$(sql_escape $PWD)%' desc 
 limit 1"
-    _histdb_query "$query"
+    suggestion=$(_histdb_query "$query")
 }
 
 ZSH_AUTOSUGGEST_STRATEGY=histdb_top_here
