b1d6325bfc1ea53da8545207892565e5b73cf687
Khaliq Gant <khaliqgant@gmail.com>
2019-04-10 16:20:26 +0200

    update query to give back the best result based on that PWD

--- zshes/histdb.zsh
+++ zshes/histdb.zsh
@@ -32,4 +32,29 @@ limit 1"
     suggestion=$(_histdb_query "$query")
 }
 
-ZSH_AUTOSUGGEST_STRATEGY=histdb_top_here
+# Query to pull in the most recent command if anything was found similar
+# in that directory. Otherwise pull in the most recent command used anywhere
+# Give back the command that was used most recently
+_zsh_autosuggest_strategy_histdb_top_fallback() {
+    local query="
+    select commands.argv from
+    history left join commands on history.command_id = commands.rowid
+    left join places on history.place_id = places.rowid
+    where places.dir LIKE 
+        case when exists(select commands.argv from history 
+        left join commands on history.command_id = commands.rowid 
+        left join places on history.place_id = places.rowid 
+        where places.dir LIKE '$(sql_escape $PWD)%' 
+        AND commands.argv LIKE '$(sql_escape $1)%') 
+            then '$(sql_escape $PWD)%' 
+            else '%'
+            end
+    and commands.argv LIKE '$(sql_escape $1)%'
+    group by commands.argv
+    order by places.dir LIKE '$(sql_escape $PWD)%' desc,
+        history.start_time desc
+    limit 1"
+    suggestion=$(_histdb_query "$query")
+}
+
+ZSH_AUTOSUGGEST_STRATEGY=histdb_top_fallback
