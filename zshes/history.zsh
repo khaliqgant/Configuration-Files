30b8ab4e05ffe969453a4307a652db69c4764b16
Khaliq Gant <khaliqgant@gmail.com>
2019-04-10 16:34:44 +0200

    add in show and search local_history commands
    * fixes https://github.com/khaliqgant/Configuration-Files/issues/1

--- zshes/history.zsh
+++ zshes/history.zsh
@@ -8,3 +8,23 @@ cp ~/.zsh_history "$historyL"/"`hostname`"
 all_history() {
     (cd "$historyL" && ack "$1")
 }
+
+# with optional limit parameter
+# the top result is the most recent
+show_local_history() {
+    limit="${1:-10}"
+    local query="
+        select history.start_time, commands.argv 
+        from history left join commands on history.command_id = commands.rowid
+        left join places on history.place_id = places.rowid
+        where places.dir LIKE '$(sql_escape $PWD)%'
+        order by history.start_time desc
+        limit $limit
+    "
+    results=$(_histdb_query "$query")
+    echo "$results"
+}
+
+search_local_history() {
+    show_local_history | ack "$1"
+}
