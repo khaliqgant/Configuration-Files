32a4af74fab9f1256e070d2efc74f943f664d372
Khaliq Gant <khaliqgant@gmail.com>
2019-06-02 13:11:05 +0200

    add trash command

--- zshes/folder-nav.zsh
+++ zshes/folder-nav.zsh
@@ -28,3 +28,6 @@ alias fd='find . -type d -name'
 # copy the current working directory to the clipboard
 alias cpwd='pwd | xclip -selection clipboard'
 alias list='ls -tlla'
+
+# https://apple.stackexchange.com/questions/50844/how-to-move-files-to-trash-from-command-line/50849#50849
+trash() { mv -fv "$@" ~/.Trash/ ; }
