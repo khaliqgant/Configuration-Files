f0b99725e185cfefae5531fadd6207003b6906fb
Khaliq <khaliqgant@gmail.com>
2021-03-16 11:18:37 +0100

    add in default php version

--- install/php-setup.sh
+++ install/php-setup.sh
@@ -3,3 +3,4 @@ $dry \curl -L -O https://github.com/phpbrew/phpbrew/releases/latest/download/php
 $dry chmod +x phpbrew.phar
 $dry sudo mv phpbrew.phar /usr/local/bin/phpbrew
 $dry phpbrew init
+$dry phpbrew install 8.0 +default +bz2=/usr/local/opt/bzip2 +zlib=/usr/local/opt/zlib +pdo +mysql
