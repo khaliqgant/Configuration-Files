4ffd3f8237efe96baec63be2766bb2bf1d17849e
Khaliq <khaliqgant@gmail.com>
2021-04-28 15:37:17 +0200

    install good as well

--- install/php-setup.sh
+++ install/php-setup.sh
@@ -4,3 +4,10 @@ $dry chmod +x phpbrew.phar
 $dry sudo mv phpbrew.phar /usr/local/bin/phpbrew
 $dry phpbrew init
 $dry phpbrew install 8.0 +default +bz2=/usr/local/opt/bzip2 +zlib=/usr/local/opt/zlib +pdo +mysql
+
+$dry phpbrew ext install gd \
+-- --with-libdir=lib/x86_64-linux-gnu \
+--with-gd=shared \
+--enable-gd-natf \
+--with-jpeg-dir=/usr \
+--with-png-dir=/usr
