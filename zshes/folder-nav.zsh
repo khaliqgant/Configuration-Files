c84828a6332b36c890cfc9596cfee1bf5c0b7d1f
Khaliq <khaliqgant@gmail.com>
2021-05-13 14:38:00 +0200

    add paris shortcut to Paris drive @ projects

--- zshes/folder-nav.zsh
+++ zshes/folder-nav.zsh
@@ -7,6 +7,7 @@ alias sites='cd ~/Sites'
 alias config='cd ~/Configuration-Files'
 alias Sites='cd ~/Sites'
 alias Config='cd ~/Configuration-Files'
+alias paris='cd /Volumes/Paris\ Drive/Projects/'
 alias spf=' cd ~/.spf13-vim-3/'
 alias home=$HOME
 alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
