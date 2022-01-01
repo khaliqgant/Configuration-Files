78514e174fec42713c7b63791d2e6941d50d1589
Khaliq <khaliqgant@gmail.com>
2022-01-01 14:49:26 +0100

    remove karabiner and update php install script

--- install/composer.sh
+++ install/composer.sh
@@ -1,6 +1,6 @@
 # read file contents into memory
 curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+sudo mv composer.phar /usr/local/bin/composer
 packages=$(<data/composers.txt)
 
 for package in $packages
