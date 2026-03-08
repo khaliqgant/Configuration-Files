5ff89fb0d1e51eaeffa2a7349faa02982dc1e5fa
Khaliq <khaliqgant@gmail.com>
2026-03-08 23:58:16 +0100

    better snap tool

--- install/app_setup.sh
+++ install/app_setup.sh
@@ -1,5 +1,12 @@
 echo "Open apps to configure them correctly"
 
+# BetterSnapTool: enable cmd+shift drag to move, opt+shift drag to resize
+echo "Configuring BetterSnapTool"
+defaults write com.hegenberg.BetterSnapTool cmdMove -bool true
+defaults write com.hegenberg.BetterSnapTool shiftMove -bool true
+defaults write com.hegenberg.BetterSnapTool optResize -bool true
+defaults write com.hegenberg.BetterSnapTool shiftResize -bool true
+
 open /Applications/RescueTime.app
 open /Applications/Next\ Meeting.app
 open /Applications/Bartender.app
