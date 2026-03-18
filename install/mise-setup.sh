1b4a9f8b7caaa251288b5de684e94875d22c23d6
Khaliq <khaliqgant@gmail.com>
2026-03-18 12:40:40 +0100

    update mise install

--- install/mise-setup.sh
+++ install/mise-setup.sh
@@ -7,17 +7,5 @@ RUBY_VERSION="${RUBY_VERSION:=3.3.0}"
 # Activate mise in current shell
 eval "$(mise activate bash)"
 
-echo "Installing Node.js $NODE_VERSION via mise"
-$dry mise use --global node@"$NODE_VERSION"
-
-echo "Installing Python $PYTHON_VERSION via mise"
-$dry mise use --global python@"$PYTHON_VERSION"
-
-echo "Installing Ruby $RUBY_VERSION via mise"
-$dry mise use --global ruby@"$RUBY_VERSION"
-
-echo "Installing Go via mise"
-$dry mise use --global go@latest
-
-echo "Installing Rust via mise"
-$dry mise use --global rust@latest
+echo "Installing languages via mise (versions defined in mise.toml)"
+$dry mise install
