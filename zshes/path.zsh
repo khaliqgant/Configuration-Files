22f0b347c39510b326477cb5ba3554a23c88922a
Khaliq <khaliqgant@gmail.com>
2026-03-23 08:52:36 +0100

    psql and git config

--- zshes/path.zsh
+++ zshes/path.zsh
@@ -5,6 +5,7 @@ eval "$(mise activate zsh)"
 
 export PATH=/usr/local/sbin:$PATH
 export PATH=/Users/Shared/DBngin/postgresql/15.1/bin:$PATH
+export PATH="/opt/homebrew/opt/postgresql@15/bin:$PATH"
 export PATH=$PATH:$HOME/bin # terraform switch
 
 [ -z "$HOSTNAME" ] && export HOSTNAME="$(hostname)"
